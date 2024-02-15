0b1f73ed4f1f6f697ee129307152aaac3e0254b9
mouatazhakkou <mouataz.hakkou@s2hgroup.com>
2024-02-15 11:50:34 +0100

    add username to user

--- entities/User.php
+++ entities/User.php
@@ -2,7 +2,7 @@
 
 class User {
 
-    public function __construct(private string $name, private string $email, private string $password, private array $roles)
+    public function __construct(private string $name, private string $email, private string $password, private array $roles, private string $username)
     {
 
     }
