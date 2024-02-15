3bfeb4d7db2f4ea465de0836433ebdf8592cabdb
mouatazhakkou <mouataz.hakkou@s2hgroup.com>
2024-02-15 11:11:32 +0100

    refactoring user class

--- entities/User.php
+++ entities/User.php
@@ -1,16 +1,9 @@
 <?php
 
 class User {
-    private $name;
-    private $email;
-    private $password;
-    private $roles;
 
-    public function __construct(string $name, string $email, string $password, array $roles)
+    public function __construct(private string $name, private string $email, private string $password, private array $roles)
     {
-        $this->name = $name;
-        $this->email = $email;
-        $this->password = $password;
-        $this->roles = $roles;
+
     }
 }
